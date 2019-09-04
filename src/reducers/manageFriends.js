6bd132a5dfa99529661fddc276fee07e13288af9
Erin Stalzer <erinstalzer@gmail.com>
2019-09-04 13:03:00 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+                let oldFriends = state.friends
+                let newFriends = [...oldFriends, action.friend]
+            return {friends: newFriends}
+
+        default:
+            return state;
+    }
 }
