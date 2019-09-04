9d891d7f162405884d63aa7c3cbf8c8b9d1d84ed
Erin Stalzer <erinstalzer@gmail.com>
2019-09-04 13:08:56 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,10 +1,17 @@
 export function manageFriends(state, action){
+    let oldFriends = state.friends
+    let newFriends = []
     switch (action.type) {
         case 'ADD_FRIEND':
-                let oldFriends = state.friends
-                let newFriends = [...oldFriends, action.friend]
-            return {friends: newFriends}
-
+                newFriends = [...oldFriends, action.friend]
+            return {friends: newFriends};
+        case 'REMOVE_FRIEND':
+                oldFriends.forEach(function (friend) {
+                    if (friend.id !== action.id) {
+                        newFriends = [...newFriends, friend]
+                    }
+                })
+            return {friends: newFriends};
         default:
             return state;
     }
