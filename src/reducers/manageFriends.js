eab3befa4dd6d94940cc185706dce4c500d20937
Kish-Siva-1 <rodes88@msn.com>
2019-09-19 15:35:01 +0200

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
 export function manageFriends(state, action){
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return (
+                {...state,
+                    friends: [
+                        ...state.friends,
+                        action.friend
+                    ]
+                }
+            )
+        case 'REMOVE_FRIEND':
+        const delete_index = state.friends.findIndex(friend => friend.id === action.id);
+            return (
+
+                {...state,
+                    friends: [
+                        ...state.friends.slice(0,delete_index),
+                        ...state.friends.slice(delete_index+1,state.friends.length)
+                    ]}
+            )
+        default: 
+            return state;
+    } 
+
 }
